748d3f5be111a89600b5f4e0f03094ef31152190
Quantum Nekomata <qnekomata@gmail.com>
2017-03-25 20:39:24 +0000

    Adding basic interpreter executable

---
+++ qclisp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace libQCLISP.qclisp
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			LispContext ctx = new LispContext ();
+			ctx.register ("+", new LispAdd ());
+			ctx.register ("range", new LispRange ());
+
+			while (true) {
+				Console.Write ("> ");
+				string n = Console.ReadLine ();
+				Console.WriteLine (ctx.evaluate (n).eval ().getString());
+			}
+		}
+	}
+}
